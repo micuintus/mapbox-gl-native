6102df4a3fcca9efa46717ab015af1079e398843
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2016-12-08 16:42:09 +0100

    [ios, macos] MGLPointFeature now conforms to NSSecureCoding

--- platform/darwin/src/MGLShape.mm
+++ platform/darwin/src/MGLShape.mm
@@ -2,6 +2,9 @@
 
 #import "MGLFeature_Private.h"
 
+#import "NSString+MGLAdditions.h"
+#import "MGLTypes.h"
+
 @implementation MGLShape
 
 + (nullable instancetype)shapeWithData:(NSData *)data encoding:(NSStringEncoding)encoding error:(NSError * _Nullable *)outError {
@@ -52,7 +55,7 @@
     if (self = [super init]) {
         _title = [coder decodeObjectOfClass:[NSString class] forKey:@"title"];
         _subtitle = [coder decodeObjectOfClass:[NSString class] forKey:@"subtitle"];
-#if !TARGET_OS_IPHONE
+#if TARGET_OS_MACOS
         _toolTip = [coder decodeObjectOfClass:[NSString class] forKey:@"toolTip"];
 #endif
     }
@@ -63,9 +66,35 @@
 {
     [coder encodeObject:_title forKey:@"title"];
     [coder encodeObject:_subtitle forKey:@"subtitle"];
-#if !TARGET_OS_IPHONE
+#if TARGET_OS_MACOS
     [coder encodeObject:_toolTip forKey:@"toolTip"];
 #endif
+}
+
+- (BOOL)isEqual:(id)other
+{
+    if (other == self) { return YES; }
+    id <MGLAnnotation> annotation = other;
+    
+#if TARGET_OS_MACOS
+    return (MGLIsEqualToString(_title, [annotation title])
+            && MGLIsEqualToString(_subtitle, [annotation subtitle])
+            && MGLIsEqualToString(_toolTip, [annotation toolTip]));
+#else
+    return (MGLIsEqualToString(_title, [annotation title])
+            && MGLIsEqualToString(_subtitle, [annotation subtitle]));
+#endif
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger hash;
+    hash += _title.hash;
+    hash += _subtitle.hash;
+#if TARGET_OS_MACOS
+    hash += _toolTip.hash;
+#endif
+    return hash;
 }
 
 - (CLLocationCoordinate2D)coordinate
