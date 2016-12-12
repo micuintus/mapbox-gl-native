405d6f261ee25c80277081117613a37f3efda1e5
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2016-12-12 16:20:32 +0100

    [ios, macos] MGLPointCollection and MGLPointCollectionFeature now conforms to NSSecureCoding

--- platform/darwin/src/MGLShape_Private.h
+++ platform/darwin/src/MGLShape_Private.h
@@ -2,6 +2,13 @@
 
 #import <mbgl/util/geojson.hpp>
 #import <mbgl/util/geometry.hpp>
+#import <mbgl/util/geo.hpp>
+
+bool compareByCoordinate(CLLocationCoordinate2D lhs, CLLocationCoordinate2D rhs);
+
+bool operator!=(const CLLocationCoordinate2D lhs, const CLLocationCoordinate2D rhs);
+
+bool operator==(std::vector<CLLocationCoordinate2D>& lhs, std::vector<CLLocationCoordinate2D>& rhs);
 
 @interface MGLShape (Private)
 
