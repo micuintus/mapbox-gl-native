6b2bf20df48bf39c9cec00e7d4cee6ca08da4822
Thiago Marcos P. Santos <thiago@mapbox.com>
2016-12-14 17:19:55 -0200

    constexpr

--- include/mbgl/util/unitbezier.hpp
+++ include/mbgl/util/unitbezier.hpp
@@ -32,7 +32,7 @@ namespace util {
 
 struct UnitBezier {
     // Calculate the polynomial coefficients, implicit first and last control points are (0,0) and (1,1).
-    constexpr UnitBezier(double p1x, double p1y, double p2x, double p2y)
+    UnitBezier(double p1x, double p1y, double p2x, double p2y)
         : cx(3.0 * p1x)
         , bx(3.0 * (p2x - p1x) - cx)
         , ax(1.0 - cx - bx)
